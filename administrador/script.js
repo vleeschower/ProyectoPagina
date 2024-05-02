923cc04f9dcc342801f71dfcc29eecd499d7b015
vleeschower <144490940+vleeschower@users.noreply.github.com>
2024-05-02 12:47:22 -0600

    Commit

--- administrador/script.js
+++ administrador/script.js
@@ -9,6 +9,7 @@ const main = document.querySelector("main");
 
 menu.addEventListener("click",()=>{
     barralateral.classList.toggle("max-barra-lateral");
+
     if(barralateral.classList.contains("max-barra-lateral"))
     {
         menu.children(0).style.display = "none";
